4787eab3203c51295d8b0150b48c2500d57c5da6
Jeeranunnat <80268940+Jeeranunnat@users.noreply.github.com>
2023-10-10 17:10:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let slide = document.querySelectorAll('.slide');
+var current = 0;
+
+function cls() {
+  for (let i = 0; i < slide.length; i++) {
+    slide[i].style.display = 'none';
+  }
+}
+
+function next() {
+  cls();
+  if (current === slide.length - 1) current = -1;
+  current++;
+  slide[current].style.display = 'block';
+  slide[current].style.opacity = 0.6;
+
+  var x = 0.6;
+  var intX = setInterval(function () {
+    x += 0.05;
+    slide[current].style.opacity = x;
+    if (x >= 1) {
+      clearInterval(intX);
+      x = 0.6;
+    }
+  }, 100);
+}
+function prev() {
+  cls();
+  if (current === 0) current = slide.length;
+  current--;
+  slide[current].style.display = 'block';
+  slide[current].style.opacity = 0.6;
+
+  var x = 0.4;
+  var intX = setInterval(function () {
+    x += 0.2;
+    slide[current].style.opacity = x;
+    if (x >= 1) {
+      clearInterval(intX);
+      x = 0.6;
+    }
+  }, 100);
+}
+
+function start() {
+  cls();
+  slide[current].style.display = 'block';
+}
+
+start();
